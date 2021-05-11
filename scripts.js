cabe755d92307909f2248341d0f5d64afb08c1b0
mdtechs <mohddanish8299@gmail.com>
2021-05-11 14:50:51 +0530

    added icons in call buttons

--- scripts.js
+++ scripts.js
@@ -3,14 +3,31 @@ checkElement('div.main') //use whichever selector you want
         var section = document.getElementById("app");
         //console.log('found');
         content =
-            '<nav class="pfx fl flc w100 p12 highlight text-center">Yunus Egg Store <span id="shopStatus" class="shopStatus closed">closed</span></nav>' +
+            '<nav class="pfx fl flc w100 p12 highlight text-center">' +
+            'Yunus Egg Store ' +
+            '<span id="shopStatus" class="shopStatus closed">' +
+            'closed' +
+            '</span>' +
+            '</nav>' +
             '<section class="prel">' +
             '<div id="lander"></div>' +
             '<canvas class="pabs b0"></canvas>' +
             '<header class="pabs t0">' +
             '<div class="fl flc w100 h100 fldcl">' +
-            '<h1 class="welcome-text text-center prel cd lhinit">1 Carat, <span class="highlight">INR <span id="rate">Loading...</span></span>' +
-            '<div class="text-center prel cd lhinit" style="color: #656565; font-size: 8pt; opacity: 0.8; padding: .5rem 1rem;">Updated on: <span id="date">Loading...</span></div>' +
+            '<h1 class="welcome-text text-center prel cd lhinit">' +
+            '1 Carat, ' +
+            '<span class="highlight">' +
+            'INR ' +
+            '<span id="rate">' +
+            'Loading...' +
+            '</span>' +
+            '</span>' +
+            '<div class="text-center prel cd lhinit" style="color: #656565; font-size: 8pt; opacity: 0.8; padding: .5rem 1rem;">' +
+            'Updated on: ' +
+            '<span id="date">' +
+            'Loading...' +
+            '</span>' +
+            '</div>' +
             '</h1>' +
             '</div>' +
             ' </header>' +
@@ -18,15 +35,39 @@ checkElement('div.main') //use whichever selector you want
             '<section class="prel fl flc fldcl">' +
             '<div class="shape"></div>' +
             '<h1 class="welcome-text text-center prel cd lhinit"><span class="highlight">Contact Us</h1>' +
-            '<div class="fl flc flww"><button data-action="whatsapp" class="prel call cp">Whatsapp</button><button data-action="call" class="prel call cp">Call</button></div>' +
+            '<div class="fl flc flww">' +
+            '<button data-action="whatsapp" class="prel call cp fl flc">' +
+            '<img src="assets/icons/whatsapp.svg" class="logo">' +
+            'Whatsapp' +
+            '</button>' +
+            '<button data-action="call" class="prel call cp fl flc">' +
+            '<img src="assets/icons/phone.svg" class="logo">' +
+            'Call YTES' +
+            '</button>' +
+            '</div>' +
             '<address class="p12 text-center">' +
             'Visit Us: ' +
             '<strong>Yunus Egg Store</strong>, ' +
             'Azad Nagar Chauraha, Campbell Road. Balaganj, Lucknow<br><br>' +
-            '<div class="tooltip">Timming: 09:00 AM to 10:00PM<span class="tooltiptext">Call Before Visit</span></div>' +
+            '<div class="tooltip">' +
+            'Timming: 09:00 AM to 10:00PM' +
+            '<span class="tooltiptext">' +
+            'Call Before Visit' +
+            '</span>' +
+            '</div>' +
             ' </address>' +
             '</section>' +
-            '<footer class="prel b0 w100 fl flc fldcl b-grey"><div>Proud To Serve You: YTES</div><div style="opacity:0.8;transform: scale(0.9)">Report Issues or Send Feedback <a href="mailto:mdtechs.contact@gmail.com">Click Here</a></div></footer>';
+            '<footer class="prel b0 w100 fl flc fldcl b-grey">' +
+            '<div>' +
+            'Proud To Serve You: YTES' +
+            '</div>' +
+            '<div style="opacity:0.8;transform: scale(0.9)">' +
+            'Report Issues or Send Feedback ' +
+            '<a href="mailto:mdtechs.contact@gmail.com">' +
+            'Click Here' +
+            '</a>' +
+            '</div>' +
+            '</footer>';
 
         section.innerHTML = content;
 
